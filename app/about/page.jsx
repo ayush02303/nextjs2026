4cc882c95c91dafdb49bb3784074d8d72501832d
ayush02303 <badolaayush295@gmail.com>
2026-07-02 07:24:36 +0530

    ss

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,11 +1,12 @@
 import Image from 'next/image'
 import React from 'react'
 import cat from "../../public/kitty.jpeg"
+import {Roboto} from "next/font/google"
 
 const page = () => {
   return (
     <div>
-      <p className='hello'>Hello from About</p>
+      <p className='hello'>Hello from About to begin</p>
 
       <Image src={"/kitty.jpeg"} width={500} height={500} alt='sample image'/>
     </div>
