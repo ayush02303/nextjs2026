5d1d0497fe7253615aaf189aa0f1b8368c84c26e
ayush02303 <badolaayush295@gmail.com>
2026-07-12 06:10:25 +0530

    s

---
+++ app/clientcomp/page.jsx
@@ -0,0 +1,16 @@
+"use client"
+
+
+import React from 'react'
+
+const client = () => {
+  return (
+    <div>
+        <h1>Hii there</h1>
+        <button onClick={()=>{ console.log("i got clicked")}}>Click Me</button>
+      
+    </div>
+  )
+}
+
+export default client
